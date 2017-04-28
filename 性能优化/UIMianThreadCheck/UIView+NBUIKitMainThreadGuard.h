e1abe1bb0e4ed16c78f2d4f90f28fff11e4b6bef
wkding <18990145302@163.com>
2017-04-28 09:17:41 +0800

    [feature] 性能优化  内存检测  和 UI主线程刷新检测

---
+++ 性能优化/UIMianThreadCheck/UIView+NBUIKitMainThreadGuard.h
@@ -0,0 +1,13 @@
+//
+//  UIView+NBUIKitMainThreadGuard.h
+//  KanPian
+//
+//  Created by wkding on 2017/4/28.
+//  Copyright © 2017年 SMIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (NBUIKitMainThreadGuard)
+
+@end
